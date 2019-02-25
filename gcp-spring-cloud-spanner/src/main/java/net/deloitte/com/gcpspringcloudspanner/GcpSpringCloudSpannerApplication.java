43336eb13ff92aecdc293f24c9bbb7e40a676973
'gaurabe' <'gaurabenath001@gmail.com'>
2019-02-25 14:27:02 +0530

    pushed gcp-spring-cloud-spanner

---
+++ gcp-spring-cloud-spanner/src/main/java/net/deloitte/com/gcpspringcloudspanner/GcpSpringCloudSpannerApplication.java
@@ -0,0 +1,13 @@
+package net.deloitte.com.gcpspringcloudspanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSpringCloudSpannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSpringCloudSpannerApplication.class, args);
+	}
+
+}
