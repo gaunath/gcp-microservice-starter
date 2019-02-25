cb2709c71fdf6f5173879f447777678e58d637b1
'gaurabe' <'gaurabenath001@gmail.com'>
2019-02-25 15:16:39 +0530

    gcp-pubsub-starter

---
+++ gcp-spring-cloud-pubsub/src/main/java/net/deloitte/com/gcpspringcloudpubsub/GcpSpringCloudPubsubApplication.java
@@ -0,0 +1,13 @@
+package net.deloitte.com.gcpspringcloudpubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSpringCloudPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpSpringCloudPubsubApplication.class, args);
+	}
+
+}
